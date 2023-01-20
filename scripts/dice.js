cd3802cbf00c38b7837cc1ff7a9d81c8af01d80c
Bogol-E <skylark@skku.edu>
2023-01-20 15:05:02 +0900

    4545

--- scripts/dice.js
+++ scripts/dice.js
@@ -5,19 +5,45 @@ function domAdd(){
 }
 */
 
-const dispDom = (bt1p, bt2p) => {
+
+const dispDom = (s1P, s2P, bt1p, bt2p) => {
     // DOM 제어
+    document.getElementById("s1").style.display = s1P;
+    document.getElementById("s2").style.display = s2P;
     document.getElementById("bt1").style.display = bt1p;
     document.getElementById("bt2").style.display = bt2p;
   }
 
-const showDice = () =>{
-    const n = Math.floor(Math.random()*6) + 1
-    const s1 = document.querySelector("#s1")
+  
 
-    s1.innerHTML = `<img src=./images/${n}.png>`
-    console.log(n)
-    dispDom("none", "block");
+const showDice = () =>{
+    const n = Math.floor(Math.random()*6) + 1;
+    const s1 = document.querySelector("#s1");
+
+    s1.innerHTML = `<img src=./images/${n}.png>`;
+    console.log(n);
+
+    const un = document.querySelectorAll("input[type=radio]");
+
+    let ox;
+    if (n == un) {
+      ox = "o";
+    } else {
+      ox = "x";
+    }
+    console.log(n, ox);
+  
+    s2.innerHTML = `<img src=./images/${ox}.png>`;
+    console.log(`사용자 선택 수 확인 => ${un}`);
+
+    
+    dispDom("block", "block", "none", "block");
 }
+
+const showOk = () => {
+    
+    window.location.reload()
+  
+  }
 
 
